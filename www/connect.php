b159704e85a7443ba5319de692939ad05d95cdea
teddyandria <teddy.andriamam@gmail.com>
2024-12-05 23:45:36 +0100

    finished

--- www/connect.php
+++ www/connect.php
@@ -1,9 +1,8 @@
 <?php
-    $host = "db";
-    $username = "root";
-    $password = "root";
-    $db = "gestion_produits";
+$host = "db";
+$username = "root";
+$password = "root_password";
+$db = "gestion_produits";
 
-    $link = mysqli_connect($host,$username,$password,$db) or die ("Erreur de connexion à la base de données.");
-    mysqli_set_charset($link,"utf8");
-?>
+$link = mysqli_connect($host, $username, $password, $db) or die("Erreur de connexion à la base de données.");
+mysqli_set_charset($link, "utf8");
